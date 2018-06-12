da0c8644e4b91093f1930208fc367b81b5af6739
dmitry mineev <duke16@mail.ru>
2018-06-12 17:25:20 +0300

    component`s lifecycle

---
+++ src/ColorPicker/Color.js
@@ -0,0 +1,34 @@
+import {Start} from './Star';
+import {StarRating} from './StarRating'
+
+export class Color extends React.Component { 
+    
+    componentWillMount() {
+        this.style = {
+            backgroundColor: "#ccc"
+        }
+    }
+    shouldComponentUpdate(nextProps) {
+        const {rating} = this.props;
+        return rating !== nextProps.rating;
+    }
+    componentWillUpdate() {
+        this.style = null;
+    }
+    componentDidUpdate(prevProps) {
+        const {title, rating} = this.props;
+        const status = (rating > prevProps.rating) ? 'better' : 'worse';
+    }
+    render() {
+        const {title, rating, color, onRate} = this.props;
+        return (
+            <section className="color" style={this.style}>
+                <h1 ref="title">{title}</h1>
+                <div className="color"
+                    style={{ backgroundColor: color }}>
+                </div>
+                <StarRating starsSelected={rating} onRate={onRate} />
+            </section>
+        )
+    }
+}
--- src/ColorPicker/ColorList.js
+++ src/ColorPicker/ColorList.js
@@ -11,17 +11,3 @@ export const ColorList = ({ colors=[], onRemove=f=>f, onRate=f=>f }) =>
             )
         }
     </div>
-
-export const Color = ({ title, color, rating=0, onRemove=f=>f, onRate=f=>f }) => 
-    <section className="color">
-        <h1>{title}</h1>
-        <button onClick={onRemove}>X</button>
-        <div className="color"
-            style={{ backgroundColor: color }}>
-        </div>
-        <div>
-            <StarRating starsSelected={rating} 
-                        onRate={onRate} 
-            />
-        </div>
-     </section>
