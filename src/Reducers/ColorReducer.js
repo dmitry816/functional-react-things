2ee0e93fb0200bfd89cc697b4baf133f4e668e20
dmitry mineev <duke16@mail.ru>
2018-06-16 19:13:58 +0300

    add rate, sort reducers

--- src/Reducers/ColorReducer.js
+++ src/Reducers/ColorReducer.js
@@ -23,9 +23,29 @@ export const color = (state={}, action) => {
 }
 
 export const colors = (state=[], action) => {
-    
+    switch (action.type) {
+        case 'ADD_COLOR':
+            return [
+                ...state,
+                color({}, action)
+            ]
+        case 'RATE_COLOR':
+            return state.map(
+                c => color(c, action)
+            )
+        case 'REMOVE_COLOR':
+            return state.filter(
+                c => c.id !== action.id
+            )
+        default:
+            return state
+    }    
     return []
 }
 export const sort = (state="SORTED_BY_DATE", action) => {
-    return ""
+    switch (action.type) {
+        case 'SORT_COLORS':
+            return action.sortBy
+        default: return state
+    }
 }
